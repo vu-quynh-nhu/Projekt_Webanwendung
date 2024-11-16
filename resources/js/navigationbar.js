fcd094346cf24ea05c775d55cf0f2f953b5d0563
quynh.vu <quynh.vu@kbst-gmbh.de>
2024-11-16 21:04:10 +0100

    added navigationbar.js

--- resources/js/navigationbar.js
+++ resources/js/navigationbar.js
@@ -1,9 +1,2 @@
 const navbar = document.querySelector(".navbar");
 const reviewerLogo = document.querySelector(".reviewer-logo");
-
-if (window.location.pathname === "../html/login.html" || window.location.pathname === "../html/signup.html") {
-    navbar.style.backgroundColor = "white";
-} else {
-    navbar.style.backgroundColor = "black";
-    reviewerLogo.style.color = "white";
-}
